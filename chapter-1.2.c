17d5698f2299524bf6e20a401a626968ea845e39
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-11-11 16:02:01 +0800

    TCPL: Chapter 1.2 - Implement Celsius-Fahrenheit table
    Convert Fahrenheit to Celsius using Integer variables.
    
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ chapter-1.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* C=(5/9)(F-32) */
+
+/* print Fahrenheit-Celsius table
+   for fahr = 0, 20, ..., 300 */
+
+int main() {
+    /* variable declaration */
+    int fahr, celsius;
+    int lower, upper, step;
+
+    /* variable assignments */
+    lower = 0;
+    upper = 200;
+    step = 20;
+
+    fahr = lower;
+
+    while(fahr < upper) {
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
